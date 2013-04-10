e356c1d7eff46c66f6c90a8cc2f1b3e575785dac
Isaac Yong <isaachawks@gmail.com>
2013-04-10 21:05:15 +0800

    Initialization of Project
    o Initialized with Eshan's code and .gitignore

---
+++ src/eshan/shafeeq/swinburne/guideme/MainActivity.java
@@ -0,0 +1,47 @@
+package eshan.shafeeq.swinburne.guideme;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends Activity {
+	private ImageButton cat, map;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		map = (ImageButton) findViewById(R.id.actMain_imBtn_map);
+		cat = (ImageButton) findViewById(R.id.actMain_imBtn_cat);
+		
+		cat.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(getApplicationContext(),Category.class);
+				startActivity(i);
+				
+			}
+		});
+		map.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(getApplicationContext(),Maps.class);
+				startActivity(i);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
---
+++ src/eshan/shafeeq/swinburne/guideme/SubCategory.java
@@ -0,0 +1,180 @@
+package eshan.shafeeq.swinburne.guideme;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class SubCategory extends ListActivity{
+	private static final String KEY_ITEM = "item"; // parent node
+	private static final String KEY_NAME = "Name";
+	private static final String KEY_URL = "url";
+	private static final String KEY_LOC ="Location";
+	private static final String KEY_DESC ="Description";
+	private static final String KEY_PRICE ="Pricerange";
+	private static final String KEY_OPEN ="Openinghours";
+	private static final String KEY_CONT ="ContactNumber";
+	private static final String KEY_LONG ="Longitude";
+	private static final String KEY_LAT ="Latitude";
+	private static final String KEY_WEB ="Website";
+	private static final String KEY_EMAIL ="Email";
+	
+	private String URL = "";
+	private String Name = "";
+	private ArrayList<HashMap<String, String>> subcats;
+	private String [] objects;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		init();
+		retrieveData();
+		
+		setListAdapter(new SubCategoryList(getApplicationContext(), R.layout.category, objects));
+	}
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+    	Intent i = new Intent(getApplicationContext(), Details.class);
+    	ArrayList<String> info = new ArrayList<String>();
+    	
+		try{
+			info.add(subcats.get(position).get(KEY_NAME).toString());
+		}catch(NullPointerException e){
+
+		}
+    	try{
+    		info.add(subcats.get(position).get(KEY_LOC).toString());
+    	}catch(NullPointerException e){
+
+    	}
+    	try{
+    		info.add(subcats.get(position).get(KEY_DESC).toString());
+    	}catch(NullPointerException e){
+
+    	}
+    	try{
+    		info.add(subcats.get(position).get(KEY_PRICE).toString());
+    	}catch(NullPointerException e){
+
+    	}
+    	try{
+    		info.add(subcats.get(position).get(KEY_OPEN).toString());
+    	}catch(NullPointerException e){
+
+    	}
+    	try{
+    		info.add(subcats.get(position).get(KEY_CONT).toString());
+    	}catch(NullPointerException e){
+
+    	}
+    	try{
+    		info.add(subcats.get(position).get(KEY_LONG).toString());
+    	}catch(NullPointerException e){
+
+    	}
+    	try{
+    		info.add(subcats.get(position).get(KEY_LAT).toString());
+    	}catch(NullPointerException e){
+
+    	}
+    	try{
+    		info.add(subcats.get(position).get(KEY_WEB).toString());
+    	}catch(NullPointerException e){
+
+    	}
+    	try{
+    		info.add(subcats.get(position).get(KEY_EMAIL).toString());
+    	}catch(NullPointerException e){
+
+    	}
+
+    	
+    	i.putStringArrayListExtra("info", info);
+    	startActivity(i);
+
+    }
+	private void retrieveData() {
+		XMLPraser parser = new XMLPraser();
+		String xml = parser.getXmlFromUrl(URL);
+		Document doc = parser.getDomElement(xml);
+		
+		NodeList nl = doc.getElementsByTagName(KEY_ITEM);
+		
+		for(int i = 0; i < nl.getLength(); i++){
+			HashMap<String, String> map = new HashMap<String, String>();
+			Element e = (Element) nl.item(i);
+			map.put(KEY_NAME, parser.getValue(e, KEY_NAME));
+			map.put(KEY_LOC, parser.getValue(e, KEY_LOC));
+			map.put(KEY_DESC, parser.getValue(e, KEY_DESC));
+			map.put(KEY_PRICE, parser.getValue(e, KEY_PRICE));
+			map.put(KEY_OPEN, parser.getValue(e, KEY_OPEN));
+			map.put(KEY_CONT, parser.getValue(e, KEY_CONT));
+			map.put(KEY_LONG, parser.getValue(e, KEY_LONG));
+			map.put(KEY_LAT, parser.getValue(e, KEY_LAT));
+			Log.d("LOL", parser.getValue(e, KEY_NAME));
+			subcats.add(map);
+		}
+		objects = new String[subcats.size()];
+		for(int i = 0; i < subcats.size(); i++){
+			objects[i] = subcats.get(i).get(KEY_NAME);
+			Log.d("LOL", objects[i]);
+		}
+		
+	}
+
+	private void init() {
+		Intent i = getIntent();
+		Name = i.getStringExtra(KEY_NAME);
+		URL = i.getStringExtra(KEY_URL);
+		subcats = new ArrayList<HashMap<String,String>>();
+		
+	}
+	public class SubCategoryList extends ArrayAdapter<String> {
+		int rand;
+		public SubCategoryList(Context context, int textViewResourceId,
+				String[] objects) {
+			super(context, textViewResourceId, objects);
+			rand = 0;
+			
+		}
+		public View getView(int position, View convertView, ViewGroup parent) {
+			
+			final int icons [] = {
+					R.drawable.green,
+					R.drawable.red,
+					R.drawable.yellow
+			};
+			
+			rand = position % 3 ;
+			LayoutInflater li = getLayoutInflater();
+			View row = li.inflate(R.layout.main_cat_list_item, parent, false);
+			TextView label = (TextView)row. findViewById(R.id.main_cat_list_item_label);
+			ImageView icon = (ImageView)row. findViewById(R.id.main_cat_list_item_icon);
+			
+			
+			label.setText(subcats.get(position).get(KEY_NAME));
+			
+			icon.setImageDrawable(getResources().getDrawable(icons[rand]));
+			
+			return row;
+		}
+	}
+	
+}

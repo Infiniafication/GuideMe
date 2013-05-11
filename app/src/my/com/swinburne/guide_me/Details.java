5659a4cb4222fc82379707d64af4e29aa3c4c7c0
Churchill Lee <feliciousx@gmail.com>
2013-05-11 14:29:50 +0800

    Testing Google Map App

--- app/src/my/com/swinburne/guide_me/Details.java
+++ app/src/my/com/swinburne/guide_me/Details.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -45,6 +47,14 @@ public class Details extends Activity{
 		// 	g += info.get(j) + "\n";
 		// }
 		// others.setText(g);
+	}
+
+	public void testMapping(View v)
+	{
+		String uri = "geo:1.556895,110.35487?z=21";
+		Intent i = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri));
+		i.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+		startActivity(i);
 	}
 
 }
